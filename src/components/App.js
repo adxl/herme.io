6b5b70e25790bd39ea697257110b7d019d273557
Adel Senhadji <adelsenhadjii@gmail.com>
2020-09-26 00:31:22 +0200

    add login POST request to API

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
 import Home from './Home';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        {/* token: true */}
-        <Switch>
-          {localStorage.getItem('token') && <Route exact path="/" component={Home} />}
-          {localStorage.getItem('token') && <Route path="/*" component={Home} />}
-        </Switch>
+	return (
+		<Fragment>
+			<BrowserRouter>
+				{/* token: true */}
+				<Switch>
+					{localStorage.getItem('token') && <Route exact path="/" component={Home} />}
+					{localStorage.getItem('token') && <Route path="/*" component={Home} />}
+				</Switch>
 
-        {/* token: false */}
-        <Switch>
-          { !localStorage.getItem('token') && <Route exact path="/login" component={Login} />}
-          { !localStorage.getItem('token') && <Route exact path="/register" component={Register} />}
-          { !localStorage.getItem('token') && <Route path="/*" component={Login} /> }
-        </Switch>
-      </BrowserRouter>
-    </>
-  );
+				{/* token: false */}
+				<Switch>
+					{ !localStorage.getItem('token') && <Route exact path="/login" component={Login} />}
+					{ !localStorage.getItem('token') && <Route exact path="/register" component={Register} />}
+					{ !localStorage.getItem('token') && <Route path="/*" component={Login} /> }
+				</Switch>
+			</BrowserRouter>
+		</Fragment>
+	);
 }
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,20 +3,54 @@ import { Link } from 'react-router-dom';
 import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
 
 class Login extends Component {
-    state = { }
+	state = {
+		username: '',
+		password: '',
+	}
 
-    render() {
+	handleInputChange = (e) => {
+		const { value } = e.target;
+		if (e.target.name === 'username') {
+			this.setState({ username: value });
+		} else {
+			this.setState({ password: value });
+		}
+	}
+
+		handleSubmit = (e) => {
+			const options = {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(this.state),
+			};
+
+			// https://herme-io.herokuapp.com/login/
+
+			e.preventDefault();
+			fetch('https://herme-io.herokuapp.com/login/', options)
+				.then((response) => response.json())
+				.then((data) => {
+					const { token } = data;
+					if (token) {
+						localStorage.setItem('token', token);
+						window.location.replace('http://localhost:3000/');
+					}
+				})
+				.catch((error) => console.log(`caught:${error}`));
+		};
+
+		render() {
     	return (
     		<Fragment>
     			<MDBContainer>
     				<MDBRow>
     					<MDBCol md="6" className="form-main">
-    						<form>
+    						<form onSubmit={this.handleSubmit}>
     							<p className="h3 text-center mb-3">Herme.io</p>
     							<p className="h4 text-center mb-4">Sign in</p>
-    							<input type="text" placeholder="Username" id="defaultFormLoginEmailEx" className="form-control" />
+    							<input name="username" type="text" placeholder="Username" id="defaultFormLoginEmailEx" className="form-control" onChange={this.handleInputChange} />
     							<br />
-    							<input type="password" placeholder="Password" id="defaultFormLoginPasswordEx" className="form-control" />
+    							<input name="password" type="password" placeholder="Password" id="defaultFormLoginPasswordEx" className="form-control" onChange={this.handleInputChange} />
     							<div className="text-center mt-4">
     								<MDBBtn color="indigo" type="submit">Login</MDBBtn>
     								<p className="mt-3">
@@ -30,7 +64,7 @@ class Login extends Component {
     			</MDBContainer>
     		</Fragment>
     	);
-    }
+		}
 }
 
 export default Login;
