117e70310805efa45663999086d445da2f596ad0
Adel Senhadji <adelsenhadjii@gmail.com>
2020-09-25 22:09:25 +0200

    add login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import {
+  MDBContainer, MDBRow, MDBCol, MDBBtn,
+} from 'mdbreact';
 
 class Login extends Component {
     state = { }
 
     render() {
       return (
-        <fragment>
-          <div>
-            <h1>Login</h1>
-          </div>
-        </fragment>
+        <Fragment>
+          <MDBContainer>
+            <MDBRow>
+              <MDBCol md="6" className="login-main">
+                <form>
+                  <p className="h3 text-center mb-3">Herme.io</p>
+                  <p className="h4 text-center mb-4">Sign in</p>
+                  <label htmlFor="defaultFormLoginEmailEx" className="grey-text">Username</label>
+                  <input type="text" id="defaultFormLoginEmailEx" className="form-control" />
+                  <br />
+                  <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">Password</label>
+                  <input type="password" id="defaultFormLoginPasswordEx" className="form-control" />
+                  <div className="text-center mt-4">
+                    <MDBBtn color="indigo" type="submit">Login</MDBBtn>
+                  </div>
+                </form>
+              </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+        </Fragment>
       );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
 import './index.css';
 
 import App from './components/App';
