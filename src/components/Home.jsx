55a67ea70f690bd492c743c6731c1269edb2439f
Adel Senhadji <adelsenhadjii@gmail.com>
2020-09-29 14:35:08 +0200

    fetch user data using auth

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,49 @@
 import React, { Component, Fragment } from 'react';
 
 class Home extends Component {
-		state = { }
+	state = {
+		user: null,
+	}
 
-		render() {
-			return (
-				<Fragment>
-					<div>
-						<h1>Home</h1>
+	componentDidMount() {
+		this.fetchUser();
+	}
 
-					</div>
-				</Fragment>
-			);
-		}
+	logout = () => {
+		localStorage.removeItem('token');
+		window.location.replace('http://localhost:3000/login');
+	}
+
+	async fetchUser() {
+		await fetch('https://herme-io.herokuapp.com/dash/', {
+			headers: {
+				Authorization: localStorage.getItem('token'),
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				this.setState({ user: data });
+			})
+			.catch((error) => console.log(`Oops: \n${error}`));
+	}
+
+	render() {
+		const { user } = this.state;
+		console.log(user);
+
+		return (
+			<Fragment>
+				<div>
+					{ user 	&&	(
+						<div>
+							<h1>Hello {user.first_name} {user.last_name}</h1>
+						</div>
+					)}
+				</div>
+
+			</Fragment>
+		);
+	}
 }
 
 export default Home;
