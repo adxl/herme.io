336dfc8296b24e59d83e09bb71538bc1b9f6b152
Adel Senhadji <adelsenhadjii@gmail.com>
2020-09-29 23:16:19 +0200

    add profile bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { MDBNav, MDBNavItem, MDBNavLink, MDBContainer, MDBRow, MDBCol } from 'mdbreact';
 
 class Home extends Component {
 	state = {
@@ -24,23 +26,39 @@ class Home extends Component {
 			.then((data) => {
 				this.setState({ user: data });
 			})
-			.catch((error) => console.log(`Oops: \n${error}`));
+			.catch((error) => console.warn(`Oops: \n${error}`));
 	}
 
 	render() {
 		const { user } = this.state;
-		console.log(user);
 
 		return (
 			<Fragment>
-				<div>
-					{ user 	&&	(
-						<div>
-							<h1>Hello {user.first_name} {user.last_name}</h1>
-						</div>
-					)}
-				</div>
-
+				<MDBNav className="justify-content-end top-nav">
+					<MDBNavLink to="#!" className="nav-link" onClick={this.logout}>Logout</MDBNavLink>
+				</MDBNav>
+				<MDBContainer fluid>
+					<MDBRow>
+						<MDBCol size="2" className="profile-col">
+							<MDBContainer className="profile-info">
+								<MDBContainer className="profile-pic">
+									<img src="https://picsum.photos/1000" alt="profile_pic" />
+								</MDBContainer>
+								<MDBContainer className="profile-name">
+									{user && `${user.first_name} ${user.last_name}`}
+								</MDBContainer>
+							</MDBContainer>
+							<MDBContainer className="profile-menu">
+								<MDBNav className="flex-column profile-nav">
+									<MDBNavLink to="#!">Home</MDBNavLink>
+									<MDBNavLink to="#!">Posts</MDBNavLink>
+									<MDBNavLink to="#!">Friends</MDBNavLink>
+								</MDBNav>
+							</MDBContainer>
+						</MDBCol>
+						<MDBCol className="feed-col ">.col-md-4</MDBCol>
+					</MDBRow>
+				</MDBContainer>
 			</Fragment>
 		);
 	}
