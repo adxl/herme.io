53afe6d6692f707a4d3315b66ebd9d9343b945c7
Adel Senhadji <adelsenhadjii@gmail.com>
2020-10-08 10:57:29 +0200

    add unique profile image to users

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -42,6 +42,7 @@ class Home extends Component {
 	render() {
 		const { user } = this.state;
 		const { currentPage } = this.state;
+		console.log(user);
 
 		return (
 			<Fragment>
@@ -53,7 +54,7 @@ class Home extends Component {
 						<MDBCol size="2" className="profile-col">
 							<MDBContainer className="profile-info">
 								<MDBContainer className="profile-pic">
-									<img src="https://picsum.photos/1000" alt="profile_pic" />
+									{user && <img src={`https://robohash.org/${user.username}`} alt="profile_pic" />}
 								</MDBContainer>
 								<MDBContainer className="profile-name">
 									{user && `${user.first_name} ${user.last_name}`}
