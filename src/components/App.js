c0408185abfc4dc42923969c6a346920fb56249c
Adel Senhadji <adelsenhadjii@gmail.com>
2020-10-20 01:24:22 +0200

    fix routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,32 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
 import Home from './Home';
+import Error404 from './Error404';
 
 function App() {
-	console.log(`${process.env.PUBLIC_URL}/*`);
+	console.log(window.location.href);
+	console.log(`/${process.env.PUBLIC_URL}/home`);
 	return (
 		<Fragment>
 			<BrowserRouter>
 				{/* token: true */}
 				<Switch>
-					{localStorage.getItem('token') && <Route path={`${process.env.PUBLIC_URL}/*`} component={Home} />}
+					{localStorage.getItem('token') && <Route exact path={`${process.env.PUBLIC_URL}/home`} component={Home} />}
+					{localStorage.getItem('token') && <Route exact path={`${process.env.PUBLIC_URL}/posts`} component={Home} />}
+					{localStorage.getItem('token') && <Route exact path={`${process.env.PUBLIC_URL}/friends`} component={Home} />}
+					{localStorage.getItem('token') && <Route component={Error404} /> }
 				</Switch>
 
 				{/* token: false */}
 				<Switch>
 					{ !localStorage.getItem('token') && <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login} />}
-					{!localStorage.getItem('token') && <Route exact path={`${process.env.PUBLIC_URL}/register`} component={Register} />}
-					{ !localStorage.getItem('token') && <Route path={`${process.env.PUBLIC_URL}/*`} component={Login} /> }
+					{ !localStorage.getItem('token') && <Route exact path={`${process.env.PUBLIC_URL}/register`} component={Register} />}
+					{/* { !localStorage.getItem('token') && <Route path="/*" component={Login} /> } */}
+					{/* { !localStorage.getItem('token') && <p>heu</p> } */}
+					{ !localStorage.getItem('token') && <Route component={Error404} /> }
+
 				</Switch>
 			</BrowserRouter>
 		</Fragment>
---
+++ src/components/Error404.jsx
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
+
+class Error404 extends Component {
+    state= { }
+
+	 render() {
+    	return (
+    		<Fragment>
+    			<h1>404</h1>
+    		</Fragment>
+    	);
+	 }
+}
+
+export default Error404;
