3232b633ab2254bc0fa159ff9200e320a5bcb340
Adel Senhadji <adelsenhadjii@gmail.com>
2020-10-20 23:35:05 +0200

    Updates

---
+++ precache-manifest.06bcaa0af96c0ab123ba40bc38221796.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df30612e0ff49285a6492c6f8fd6ca0",
+    "url": "/herme.io/index.html"
+  },
+  {
+    "revision": "45aee8d64990dcb0ae1f",
+    "url": "/herme.io/static/css/2.1c7342ec.chunk.css"
+  },
+  {
+    "revision": "95024fe2851be8d95f61",
+    "url": "/herme.io/static/css/main.afbd9fde.chunk.css"
+  },
+  {
+    "revision": "45aee8d64990dcb0ae1f",
+    "url": "/herme.io/static/js/2.fd59fb02.chunk.js"
+  },
+  {
+    "revision": "4557c15fd8984004468e8d016ab0ee8b",
+    "url": "/herme.io/static/js/2.fd59fb02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95024fe2851be8d95f61",
+    "url": "/herme.io/static/js/main.6ef12133.chunk.js"
+  },
+  {
+    "revision": "f5024a7ecaefc82fbc42",
+    "url": "/herme.io/static/js/runtime-main.54949a17.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/herme.io/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/herme.io/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/herme.io/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/herme.io/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/herme.io/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/herme.io/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/herme.io/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/herme.io/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/herme.io/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/herme.io/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/herme.io/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/herme.io/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/herme.io/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/herme.io/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/herme.io/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/herme.io/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/herme.io/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
